241f8a92f82799719219fcd58d9c09bb61d626f5
bruno <brunotheodoro123@gmail.com>
2022-08-07 15:49:48 -0300

    fix get

--- src/infra/repositories/DonationsRepository.ts
+++ src/infra/repositories/DonationsRepository.ts
@@ -12,7 +12,7 @@ class DonationsRepository implements IDonationRepository {
   async list(): Promise<any> {
     const donations = await this.prisma.donation.findMany({
       orderBy: {
-        id: "desc",
+        id: "asc",
       },
       select: {
         id: true,
